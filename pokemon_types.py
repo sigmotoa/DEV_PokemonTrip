23698944b343191dffab1640459ad3e3c6923fac
sigmotoa <si.galvismotoa@gmail.com>
2026-04-09 15:14:55 -0500

    [ADD] model pokemon base and pokemonID

---
+++ pokemon_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class PokemonType(str,Enum):
+    HIERBA = "hierba"
+    FUEGO = "fuego"
+    ELECTRICO = "electrico"
+    NORMAL = "normal"
+    LUCHA = "lucha"
+    VENENO = "veneno"
+    AGUA = "agua"
+    FANTASMA = "fantama"
