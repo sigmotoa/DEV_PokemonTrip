fb7ce849e608a08441021ebbd6902eb62ab10004
sigmotoa <si.galvismotoa@gmail.com>
2026-04-28 15:42:59 -0500

    [ADD] Find one trainer

--- operations/operations_trainer_db.py
+++ operations/operations_trainer_db.py
@@ -1,3 +1,4 @@
+from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
 from models.trainer import TrainerBase, TrainerID
 
@@ -9,3 +10,8 @@ def createTrainer(trainer: TrainerBase, session: Session):
     session.refresh(new_trainer)
     return new_trainer
 
+def findTrainer(id:int, session: Session):
+    try:
+        return session.get_one(TrainerID, id)
+    except NoResultFound:
+        return None
